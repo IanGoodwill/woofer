6edec469e5c8246001d529dbbb882d92d33150f6
Ian <iangoodwill2@gmail.com>
2020-04-07 20:10:23 -0600

    moved js from index.blade.php to script.js in js folder

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -11,7 +11,6 @@
 <br>
 <br>
 
-
 <div class="float-right" class="mt-5">
 
 <h2> Welcome, {{ $profile->username }} </h2>
@@ -20,7 +19,6 @@
     <img class="rounded-circle z-depth-2"  class="img-responsive" src="{{ $post->picture }}" alt="Profile picture" style="width:40%" />
 </figure>
 
-
 <a href="{{ route('profiles.edit', $profile->id) }}" class="navbar-brand" class="nav-link" class="text-center" >Edit Profile</a>
 
 <br>
@@ -28,6 +26,5 @@
 <a href="{{ route('profiles.show', $profile->id) }}" class="navbar-brand" class="nav-link" class="text-center" >View Profile</a>
 
 <br>
-
 
 </div> 
